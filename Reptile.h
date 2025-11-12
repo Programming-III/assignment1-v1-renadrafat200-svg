cba641e1f25109decda4cbc3147eb73dcbea127f
renadrafat200-svg <renadrafat2007@gmail.com>
2025-11-12 15:19:54 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,6 +5,18 @@
 
 //define reptile class here
 
+#include<iostream>;
+
+include<string>;
+class Reptile :public Animal {
+private:
+	bool isVenomous;
+public:
+	Reptile();
+	Reptile(bool isVenomous, String name, int age, bool isHungry);
+	~Reptile();
+
+}
 
 
 
