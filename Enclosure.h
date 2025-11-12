a09aac03b3d3968fa478b8eaec3bc210ffecfdd6
renadrafat200-svg <renadrafat2007@gmail.com>
2025-11-12 15:17:00 +0200

    Implement Enclosure class with animal management
    Added Enclosure class with methods for managing animals.

--- Enclosure.h
+++ Enclosure.h
@@ -5,6 +5,23 @@
 
 //define enclosure class here 
 
+#include<iostream>;
+include<string>;
+
+class Enclosure :public Animal {
+private:
+	Animal* animal;
+	int capacity;
+	int currentCount;
+public:
+	void addAnimal(Animal* a);
+	void displayAnimals();
+	Enclosure();
+	Enclosure(Animal* animal, int capacity, int currentCount, String name, int age, bool isHungry);
+	~Enclosure();
+}
+#endif
+
 
 
 
