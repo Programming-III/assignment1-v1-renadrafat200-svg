556b942bc1d252684a9daa6ef54dc560fa7ee08d
renadrafat200-svg <renadrafat2007@gmail.com>
2025-11-12 15:13:42 +0200

    Define Animal class with basic attributes and methods
    Added Animal class with constructors, methods, and member variables.

--- Animal.h
+++ Animal.h
@@ -5,6 +5,25 @@
 using namespace std;
 
 //define animal class here 
+class Animal {
+private :
+	String name;
+	int age;
+	bool isHungry;
+public:
+	Animal();
+	Animal(String name,int age,bool isHungry);
+	void display();
+	void feed();
+	void setname();
+	void setage();
+	String getname();
+	int getage();
+	bool getH();
+	~Animal();
+
+};
+#endif 
 
 
 
