1a137f93b2f0a22ac12f98a790ed97b3b98385fa
renadrafat200-svg <renadrafat2007@gmail.com>
2025-11-12 15:21:18 +0200

    Define Visitor class with attributes and methods
    Added visitor class with constructor and display method.

--- Visitor.h
+++ Visitor.h
@@ -4,6 +4,19 @@
 #include <string>
 using namespace std;
 
+#include<iostream>;
+include<string>;
+class Visitor {
+private:
+	String visitorName;
+	int TicketBought;
+public:
+	Visitor();
+	Visitor(String visitorName, int TicketBought);
+	void displayInfo();
+}
+#endif
+
 //define visitor class here
 
 
